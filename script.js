fc4a0b950af75c7c0120a397bf64e741919bc08d
rizk <rizkdotai@gmail.com>
2024-01-13 11:28:18 +0200

    Add base styles

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 const searchInput = document.getElementById('searchInput')
 const fetchButton = document.getElementById('fetchWeather')
-const temperature = document.querySelector('.temperature') 
-const description = document.querySelector('.description')
+const temperature = document.getElementById('temp-div') 
+const description = document.getElementById('.description')
+const weatherIcon = document.getElementById('weather-icon');
 
 
 
@@ -10,6 +11,11 @@ const description = document.querySelector('.description')
 
 
 async function fetchTemperature() {
+
+    temperature.textContent = '';
+    description.textContent = '';
+    
+
     const searchKeyword = searchInput.value || 'London'
 
     try {           /* `http://api.weatherapi.com/v1/current.json?key=85a9aed9412947e9b68160302240801&q=Egypt&aqi=no` , {mode:'cors'}*/
@@ -21,8 +27,12 @@ const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
 
     const responseData = await response.json();
     console.log(responseData)
+   const cityName = responseData.name 
    temperature.textContent = Math.round(responseData.main.temp - 273.15)
    description.textContent = responseData.weather[0].description
+   const iconCode = responseData.weather[0].icon;
+   const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+   
     }
     catch(error) {
         console.log('Error fetching info:', error)
